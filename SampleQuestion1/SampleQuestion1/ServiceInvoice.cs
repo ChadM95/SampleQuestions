baa0c7e6ddf9a54aac7e3db2d82dfb6ce656325f
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-29 20:03:49 +0000

    add class ServiceInvoice / add properties and constants

---
+++ SampleQuestion1/SampleQuestion1/ServiceInvoice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace SampleQuestion1
+{
+	public class ServiceInvoice
+	{
+		//constants
+		const int LABOUR_COST = 45;
+		const double PARTS_VAT = 0.135;
+		const double LABOUR_VAT = 0.215;
+
+		//properties
+		public string Name { get; set; }
+		public string Registration { get; set; }
+		public int MilesOnClock { get; set; }
+		public int HoursLabour { get; set; }
+		public int PartsCost { get; set; }
+
+
+
+		//ctor
+		public ServiceInvoice()
+		{
+		}
+
+
+		//methods
+
+
+
+	}
+}
+
