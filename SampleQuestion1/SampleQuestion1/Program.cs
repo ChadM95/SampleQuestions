ab7d5cd1adb10f8f7d198a2e7e4158bd25c6da3c
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-29 20:15:10 +0000

    add user input

--- SampleQuestion1/SampleQuestion1/Program.cs
+++ SampleQuestion1/SampleQuestion1/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
+        //setup variables
+        string name, registration;
+        int miles, hours, partsCost;
+
+
+        //input
+        Console.Write("Enter name> ");
+        name = Console.ReadLine();
+
+        Console.Write("Enter car registration> ");
+        registration = Console.ReadLine();
+
+        Console.Write("Enter miles on clock> ");
+        miles = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter hours labour> ");
+        hours = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter cost of parts> ");
+        partsCost = int.Parse(Console.ReadLine());
+
+
+
+
+        //create object
+
+
+        //calculations / methods
+
+
         Console.ReadLine();
     }
 }
