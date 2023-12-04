f32470e41d9ae8cce6fdfe93e69de1c6e3a03b00
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-12-04 19:16:45 +0000

    add class , properties and ctor

---
+++ SampleQuestion1/SampleQuestion2/PaySlip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SampleQuestion2
+{
+	public class PaySlip
+	{
+		//properties
+		public string Name { get; set; }
+		public int RateOfPay { get; set; }
+		public int Hours { get; set; }
+
+
+		public PaySlip(string name, int rateOfPay, int hours)
+		{
+			Name = name;
+			RateOfPay = rateOfPay;
+			Hours = hours;
+		}
+
+	}
+}
+
