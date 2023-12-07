58a7a7a70fc3130f56ed6798271a5ce370eed8af
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-12-07 20:47:38 +0000

    create basic class with tostring

--- SampleQuestion1/SampleQuestion3/ElectricityBill.cs
+++ SampleQuestion1/SampleQuestion3/ElectricityBill.cs
@@ -1,22 +1,36 @@
+using System.Security.Principal;
+
 namespace ConsoleApp1;
 
 public class ElectricityBill
 {
-    public string name { get; set; }
-    public TYPE Type { get; set; }
+  //properties
+    public string Name { get; set; }
+    public string AccountNo { get; set; }
+    public int DayReadingCurrent { get; set; }
+    public int DayReadingPrevious { get; set; }
+    public int NightReadingCurrent { get; set; }
+    public int NightReadingPrevious { get; set; }
 
-    
-    
-//ctors
+  //ctors
 
-    public ElectricityBill()
+    public ElectricityBill(string name, string accNo, int dayReadingCurrent, int dayReadingPrevious, int nightReadingCurrent, int nightReadingPrevious)
     {
-            
+        Name = name;
+        AccountNo = accNo;
+        DayReadingCurrent = dayReadingCurrent;
+        DayReadingPrevious = dayReadingPrevious;
+        NightReadingCurrent = nightReadingCurrent;
+        NightReadingPrevious = nightReadingPrevious;
     }
 
-//methods
-    
-    
-    
+    //methods
+    public override string ToString()
+    {
+        return $"{Name} {AccountNo}";
+    }
+
+
+
 }
 
