27d34285eb110b299c767ca27840ff031157a185
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-12-07 20:40:54 +0000

    rename to samplequestion4

---
+++ SampleQuestion1/SampleQuestion3/ElectricityBill.cs
@@ -0,0 +1,22 @@
+namespace ConsoleApp1;
+
+public class ElectricityBill
+{
+    public string name { get; set; }
+    public TYPE Type { get; set; }
+
+    
+    
+//ctors
+
+    public ElectricityBill()
+    {
+            
+    }
+
+//methods
+    
+    
+    
+}
+
