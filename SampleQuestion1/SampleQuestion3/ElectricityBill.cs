c7b731fe562390731468597b634e6215243d37eb
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-12-09 18:24:38 +0000

    add getUnits methods

--- SampleQuestion1/SampleQuestion3/ElectricityBill.cs
+++ SampleQuestion1/SampleQuestion3/ElectricityBill.cs
@@ -5,6 +5,9 @@ namespace ConsoleApp1;
 
 public class ElectricityBill
 {
+    //constants
+    decimal DAY_UNIT = 0.42m, NIGHT_UNIT = 0.21m, VAT = 0.135m;
+
   //properties
     public string Name { get; set; }
     public string AccountNo { get; set; }
@@ -37,10 +40,12 @@ public class ElectricityBill
         output += string.Format(formatter,"Previous Day Reading :", DayReadingPrevious);
         output += string.Format(formatter,"Current Night Reading :", NightReadingCurrent);
         output += string.Format(formatter,"Previous Night Reading :", NightReadingPrevious);
+        output += string.Format(formatter,"Day Units Used :", GetDayUnits());
+        output += string.Format(formatter, "Night Units Used :", GetNightUnits());
         return output;
     }
 
-    public static void GetUserInput(out string name, out string accNo, out int dayReadingCurrent, out int dayReadingPrevious, out int nightReadingCurrent, out int nightReadingPrevious)
+    public void GetUserInput(out string name, out string accNo, out int dayReadingCurrent, out int dayReadingPrevious, out int nightReadingCurrent, out int nightReadingPrevious)
     {
         Console.Write("Enter Name >> ");
         name = Console.ReadLine();
@@ -55,6 +60,20 @@ public class ElectricityBill
         Console.Write("Enter Previous Night Reading >> ");
         nightReadingPrevious = int.Parse(Console.ReadLine());
     }
+
+    //work out day and night units by subtracting previous values from current ones
+    //calculate percentage use day / night
+
+    public int GetDayUnits()
+    {
+        return DayReadingCurrent - DayReadingPrevious;
+    }
+
+    public int GetNightUnits()
+    {
+        return NightReadingCurrent - NightReadingPrevious;
+    }
+
 
 }
 
