a068797e11992242673c0e8e7f123427007f17ca
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-12-11 19:16:46 +0000

    move GetUserInput method to program.cs file

--- SampleQuestion1/SampleQuestion3/Program.cs
+++ SampleQuestion1/SampleQuestion3/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
             int dayReadingCurrent, dayReadingPrevious, nightReadingCurrent, nightReadingPrevious;
 
             //get user input 
-            ElectricityBill.GetUserInput(out name, out accNo, out dayReadingCurrent, out dayReadingPrevious, out nightReadingCurrent, out nightReadingPrevious);
+            GetUserInput(out name, out accNo, out dayReadingCurrent, out dayReadingPrevious, out nightReadingCurrent, out nightReadingPrevious);
 
             //create object
             ElectricityBill eb1 = new ElectricityBill(name,accNo,dayReadingCurrent,dayReadingPrevious,nightReadingCurrent,nightReadingPrevious);
@@ -25,7 +25,24 @@ namespace ConsoleApp1
             Console.ReadLine();
 
         }
-        static void DisplayColourBars(ElectricityBill eb)
+
+        public static void GetUserInput(out string name, out string accNo, out int dayReadingCurrent, out int dayReadingPrevious, out int nightReadingCurrent, out int nightReadingPrevious)
+        {
+            Console.Write("Enter Name >> ");
+            name = Console.ReadLine();
+            Console.Write("Enter Account Number >> ");
+            accNo = Console.ReadLine();
+            Console.Write("Enter Current Day Reading >> ");
+            dayReadingCurrent = int.Parse(Console.ReadLine());
+            Console.Write("Enter Previous Day Reading >> ");
+            dayReadingPrevious = int.Parse(Console.ReadLine());
+            Console.Write("Enter Current Night Reading >> ");
+            nightReadingCurrent = int.Parse(Console.ReadLine());
+            Console.Write("Enter Previous Night Reading >> ");
+            nightReadingPrevious = int.Parse(Console.ReadLine());
+        }
+
+        public static void DisplayColourBars(ElectricityBill eb)
         {
             int day = (int)(eb.GetDayPercent() * 100);
             int night = (int)(eb.GetNightPercent() * 100);
